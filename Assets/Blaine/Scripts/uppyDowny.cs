d6f19c0479a7ff4cf900d49364cbbd70c47ab4ee
Thomas <ThomasJohnWolfe@gmail.com>
2014-02-24 16:00:16 -0800

    Combined Blaine's art package for week 7

---
+++ Assets/Blaine/Scripts/uppyDowny.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class uppyDowny : MonoBehaviour {
+	
+	public float moveness = 1;
+	public float speed = 1;
+	
+	private Vector3 initLoc;
+	
+	// Use this for initialization
+	void Start () {
+		initLoc = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = initLoc + new Vector3(0,Mathf.Sin(Time.time*speed)*moveness,0);
+	}
+}
---
+++ Assets/Sample Assets/ResetPos.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetPos : MonoBehaviour {
+	public GameObject startPos;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			GameObject.FindGameObjectWithTag("Player").transform.position = startPos.transform.position;;
+			GameObject.FindGameObjectWithTag("Player").transform.rotation = startPos.transform.rotation;
+			GameObject.FindGameObjectWithTag("Player").rigidbody.velocity = Vector3.zero;
+		}
+	}
+}
