9aa6311d718fa33ea8f9d720e7478d6ba644b115
unknown <student@AI231LAB0612P19.AICASF.EDMC.STU>
2014-03-17 19:54:36 -0700

    combined blaines week 10

--- Assets/Blaine/Scripts/uppyDowny.cs
+++ Assets/Blaine/Scripts/uppyDowny.cs
@@ -5,16 +5,18 @@ public class uppyDowny : MonoBehaviour {
 	
 	public float moveness = 1;
 	public float speed = 1;
+	public float offset = 0;
 	
 	private Vector3 initLoc;
 	
 	// Use this for initialization
 	void Start () {
 		initLoc = this.transform.position;
+		offset = Random.Range (0, 100);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		this.transform.position = initLoc + new Vector3(0,Mathf.Sin(Time.time*speed)*moveness,0);
+		this.transform.position = initLoc + new Vector3(0,Mathf.Sin((Time.time*speed)+offset)*moveness,0);
 	}
 }
