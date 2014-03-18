f2a17f20d9ea1eb4b9d43537de693631fb8918dc
unknown <student@AI231LAB0612P19.AICASF.EDMC.STU>
2014-03-17 21:22:22 -0700

    light enable script

---
+++ Assets/RacingGame/Scripts/LightEnabler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightEnabler : MonoBehaviour {
+
+	public 	string 			lightTagName 	= "PointLight";
+	public 	string 			playerName 		= "Player";
+	public 	float 			enableDistance	= 150f;
+	public	float			forwardDistance	= 50f;
+			Transform 		player;
+			GameObject[] 	pointLights;
+
+
+	void Start () {
+		pointLights = GameObject.FindGameObjectsWithTag (lightTagName);
+	}
+	
+
+	void Update () {
+	
+	}
+
+	void FixedUpdate(){
+		player = GameObject.FindGameObjectWithTag (playerName).transform;
+		if (player != null) {
+			foreach(GameObject light in pointLights){
+				light.SetActive(Vector3.Distance(player.position+player.transform.forward*forwardDistance,light.transform.position)<enableDistance);
+			}
+		}
+	}
+}
