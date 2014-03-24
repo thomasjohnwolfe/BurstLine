00d067be751402508657051aadbdb48d4d8e651e
Thomas <ThomasJohnWolfe@gmail.com>
2014-03-23 21:34:39 -0700

    added blade particle and sound, needs extra check on line 20 of blade projectile

--- Assets/RacingGame/Scripts/Hieu/ProjectileScript/BladeProjectile.cs
+++ Assets/RacingGame/Scripts/Hieu/ProjectileScript/BladeProjectile.cs
@@ -2,20 +2,40 @@
 using System.Collections;
 
 public class BladeProjectile : ProjectileScript {
+	static GameObject particle1,particle2;
+	public string particle1name = "BladeParticle1";
+	public string particle2name = "BladeParticle2";
 
-	// Use this for initialization
 	void Start () {
+		particle1 = particle1==null? GameObject.Find(particle1name):particle1;
+		particle2 = particle2==null? GameObject.Find(particle2name):particle2;
+		particle1.SetActive(false);
+		particle2.SetActive(false);
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
 	}
 
 	void OnTriggerEnter(Collider c){
 		if(c.gameObject.tag == "OtherPlayer"){
+			StartCoroutine(ActivateParticle(GetFreeParticle()));
 			c.gameObject.GetComponent<CarRally>().dealDamage(damage);
 			//Debug.Log(this.owner+" deal "+damage+" to "+c.gameObject.name);
 		}
+	}
+
+	GameObject GetFreeParticle(){
+		return particle1.activeSelf ? particle2 : particle1;
+	}
+
+	IEnumerator ActivateParticle(GameObject particle){
+		particle.SetActive(true);
+		GameSoundCommands.instance.PlayShoot();
+		particle.transform.position = transform.position;
+		particle.transform.rotation = transform.rotation;
+		yield return new WaitForSeconds(2f);
+		particle.SetActive(false);
 	}
 
 }
